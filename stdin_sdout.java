e254c4751b714fc8c3b56a92f476740036d10422
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-01-02 21:13:12 +0530

    day 1

---
+++ stdin_sdout.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+public class stdin_sdout {
+    public static void main(String[] args) {
+          Scanner sc= new Scanner(System.in);
+          int a,b;
+          a=sc.nextInt();
+          System.out.println(a);
+          b=sc.nextInt();
+          System.out.println(b);
+        
+    }
+}
