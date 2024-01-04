bb0f91baac1d0d5b9050c9527f3ca1c80b250ed5
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-01-04 16:14:47 +0530

    Day-3

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class datatypes {
+    public static void main(String[] args) {
+        String mystring1 = "InterviewBit";
+        String mystring2 = "Don't change the variable name";
+        int myint = 11;
+        boolean myboolean = true;
+
+        System.out.println("String1 = " + mystring1);
+        System.out.println("String2 = " + mystring2);
+        System.out.println("int = " + myint);
+        System.out.println("boolean = " + myboolean);
+
+    }
+}
