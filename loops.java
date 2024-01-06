7ab99ae481bb3ca1483ab683204f5d1e8584dc08
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-01-06 14:27:41 +0530

    day-5

---
+++ loops.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class loops {
+    public static void main(String[] args) {
+        /*** Don't change the code here ***/
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        inp.close();
+        /***********************************/
+
+        /*** Your code goes here ***/
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
