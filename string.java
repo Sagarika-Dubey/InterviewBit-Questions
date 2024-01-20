3c222d73319b057192510345e8dcc915bb0e58e2
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-01-20 23:19:39 +0530

    Day 19

---
+++ string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public interface string {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        int l = a.length() + b.length();
+        System.out.println(l);
+        int res = a.compareTo(b);
+        if (res < 0) {
+            System.out.println("No");
+        } else if (res >= 1) {
+            System.out.println("Yes");
+        }
+        String A = a.toUpperCase() + " " + b.toUpperCase();
+        System.out.println(A);
+    }
+}
