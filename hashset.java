d2ce170c169c2815d152e6e9a73e2b7ce64c8bab
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-01-26 16:56:08 +0530

    Day 25

---
+++ hashset.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class hashset {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> set = new HashSet<Integer>();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (!set.contains(arr[i])) {
+                set.add(arr[i]);
+            }
+        }
+        System.out.println(set.size());
+    }
+}
